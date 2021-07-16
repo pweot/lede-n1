927453a4b4c295e655304ab57b2424580ff5ae58
imapproxy <40516405+imapproxy@users.noreply.github.com>
2021-07-17 00:11:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/tuanqing/install-program package/install-program
-git clone https://github.com/kongfl888/luci-app-adguardhome package/adguardhome
