4ad8b6a887d40e9b9c6156fb5be5c0413fd81849
imapproxy <40516405+imapproxy@users.noreply.github.com>
2021-08-01 12:41:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,7 @@
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc package/luci-app-turboacc
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant package/luci-app-fileassistant
 #echo 'src-git smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
 #git clone https://github.com/vernesong/OpenClash.git package-temp
 #mv -f package-temp/luci-app-openclash package/lean/
